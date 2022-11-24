58d179c92a8ade984e7f5e87a36e21d871435546
kalyaharish <kalyaharish@gmail.com>
2022-11-24 20:28:00 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,22 @@
+const hourEl=document.getElementById("hour");
+const minutesEl=document.getElementById("minutes");
+const secondsEl=document.getElementById("seconds")
+const ampmEl=document.getElementById("ampm")
+function UpdateClock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM"
+    if(h>12){
+        h=h-12
+        ampm="PM"
+    }
+    hourEl.innerText=h
+    minutesEl.innerText=m
+    secondsEl.innerText=s
+    ampmEl.innerText=ampm
+    setTimeout(()=>{
+        UpdateClock()
+    },1000)
+}
+UpdateClock()
